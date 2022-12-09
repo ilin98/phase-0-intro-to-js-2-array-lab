96d015cd222205baeb175e8c9b6cfd932d32cce7
Isaac <isaac.lin.pnc@gmail.com>
2022-12-09 13:47:06 -0500

    commit tree ce819334a1e15f0fbaa3707cfcb2642be7962f47

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(name) {
+    let newArray = [...cats.slice(), name]
+    return newArray;
+}
+
+function prependCat(name) {
+    let newArray = [name, ...cats.slice()];
+    return newArray;
+}
+
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1)
+}
