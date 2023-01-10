f464447562fd88858f9bfbc0bff66ebf6be8cd7d
Isaac <isaac.lin.pnc@gmail.com>
2023-01-10 10:50:52 -0500

    finished lab.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+let cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(name) {
+    let newCats = [...cats, name];
+    return newCats;
+};
+
+function prependCat(name) {
+    let newCats = [name, ...cats];
+    return newCats;
+}
+
+function removeLastCat() {
+    let newCats = [...cats.slice(0, -1)];
+    return newCats;
+}
+
+function removeFirstCat() {
+    let newCats = [...cats.slice(1)];
+    return newCats;
+}
